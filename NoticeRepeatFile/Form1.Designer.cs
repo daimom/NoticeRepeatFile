1a4a8379e54be0639fd5c26611c3f3576b8025ab
daimom <daimom@DAIMOM-Home>
2017-01-01 21:18:25 +0800

    監控完成

--- NoticeRepeatFile/Form1.Designer.cs
+++ NoticeRepeatFile/Form1.Designer.cs
@@ -28,21 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.txtPath = new System.Windows.Forms.TextBox();
             this.dg1 = new System.Windows.Forms.DataGridView();
             this.txtMsg = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.txtKeyword = new System.Windows.Forms.TextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.dg1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(372, 60);
+            this.button1.Location = new System.Drawing.Point(177, 58);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Search";
+            this.button1.Text = "Import";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -51,7 +54,7 @@
             this.txtPath.Location = new System.Drawing.Point(12, 60);
             this.txtPath.Name = "txtPath";
             this.txtPath.ReadOnly = true;
-            this.txtPath.Size = new System.Drawing.Size(354, 22);
+            this.txtPath.Size = new System.Drawing.Size(159, 22);
             this.txtPath.TabIndex = 2;
             // 
             // dg1
@@ -73,19 +76,32 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(473, 60);
+            this.button2.Location = new System.Drawing.Point(435, 57);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 5;
-            this.button2.Text = "Select";
+            this.button2.Text = "Search";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtKeyword
+            // 
+            this.txtKeyword.Location = new System.Drawing.Point(259, 58);
+            this.txtKeyword.Name = "txtKeyword";
+            this.txtKeyword.Size = new System.Drawing.Size(170, 22);
+            this.txtKeyword.TabIndex = 6;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(689, 343);
+            this.Controls.Add(this.txtKeyword);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.txtMsg);
             this.Controls.Add(this.dg1);
@@ -94,6 +110,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
             ((System.ComponentModel.ISupportInitialize)(this.dg1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -107,6 +124,8 @@
         private System.Windows.Forms.DataGridView dg1;
         private System.Windows.Forms.TextBox txtMsg;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtKeyword;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
     }
 }
 
--- NoticeRepeatFile/Form1.cs
+++ NoticeRepeatFile/Form1.cs
@@ -18,11 +18,20 @@ namespace NoticeRepeatFile
     {
         private SQLiteConnection sqlite_connect;
         private SQLiteCommand sqlite_cmd;
+        FileSystemWatcher _watch = new FileSystemWatcher();
         public Form1()
         {
             InitializeComponent();
         }
-
+        public class fileData
+        {
+            public string sourceName { get; set; }
+            public string fileName { get; set; }
+            public string location { get; set; }
+            public string fileTime { get; set; }
+        }
+        #region 控制項事件
+   
         private void Form1_Load(object sender, EventArgs e)
         {
             if (!File.Exists(Application.StartupPath + @"\fileLibary.db"))
@@ -40,16 +49,120 @@ namespace NoticeRepeatFile
                 //INTEGER PRIMARY KEY AUTOINCREMENT=>auto increase index
                 sqlite_cmd.ExecuteNonQuery(); //using behind every write cmd
             }
+            _watch.Path = @"J:\torrent";
+            _watch.Filter = "*.torrent";
+            _watch.EnableRaisingEvents = true;
+            //觸發事件
+            _watch.Created += new FileSystemEventHandler(watch_Created);
+
+            //指定使用的容器
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);           
+            //建立NotifyIcon
+            this.notifyIcon1.Icon = new Icon("alert.ico");
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.Text = "監控中....";
+            this.notifyIcon1.MouseDoubleClick += doubleClick;
         }
-        public class fileData
+
+        private void doubleClick(object sender, MouseEventArgs e)
         {
-            public string sourceName { get; set; }
-            public string fileName { get; set; }
-            public string location { get; set; }
-            public string fileTime { get; set; }
+            UpdateForm();
         }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                this.notifyIcon1.Visible = true;
+            }
+        }
+        
+        private void watch_Created(object sender, FileSystemEventArgs e)
+        {
+            List<fileData> listFile = new List<fileData>();
+            var dirInfo = new DirectoryInfo(e.FullPath.ToString());
+            var fileName = dirInfo.Name;
+            string txtKeyWord = fileName;
+            string[] keyword = txtKeyWord.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var name in keyword)
+            {
+                string txtKey = name;
+                if (name.IndexOf(".torrent") >= 0)
+                    txtKey = name.Replace(".torrent", "");
+                var result = searchKeyword(txtKey);
+                if (result.Result.Count() > 0)
+                {
+                    var oneFile = result.Result.ToList();
+                    listFile.AddRange(oneFile);
+                }
+                    
+            }
+            if(listFile.Count> 0)
+            {                
+                notifyIcon1.Visible = true;
+                notifyIcon1.BalloonTipText = "警告！！有檔案重複。";
+                notifyIcon1.BalloonTipTitle = "警告！！";
+                notifyIcon1.ShowBalloonTip(5000);                
+                UpdateForm();                
+            }
+                
+            UpdateUI(listFile);
+            //dg1.DataSource = listFile;
+
+        }
+        
+        private delegate void UpdateFormCallBack();
+        private void UpdateForm()
+        {
+            if (this.InvokeRequired)
+            {
+                UpdateFormCallBack uu = new UpdateFormCallBack(UpdateForm);
+                this.Invoke(uu);
+            }
+            else
+            {
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+        private delegate void UpdateUICallBack(List<fileData> listFile);
+        private void UpdateUI(List<fileData>  listFile)
+        {
+            if (this.InvokeRequired)
+            {
+                UpdateUICallBack uu = new UpdateUICallBack(UpdateUI);
+                this.Invoke(uu,listFile);
+            }
+            else
+            {
+                dg1.DataSource = listFile;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            replaceFile();
+            //testRegex();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //string txtKeyWord = txtKey.Text;
+            //string[] keyword = txtKeyWord.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            //foreach (var a in keyword)
+            //{
+            //listMsg.Items.Add(a);
+            string a = txtKeyword.Text;
+            var result = searchKeyword(a);
+            if (result.Result.Count() > 0)
+                dg1.DataSource = result.Result.ToList();
+
+            //}
+        }
+        #endregion
         public void replaceFile()
         {
+            List<fileData> listFile = new List<fileData>();
             FolderBrowserDialog openFolderDialog = new FolderBrowserDialog();
 
             if (openFolderDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
@@ -58,20 +171,112 @@ namespace NoticeRepeatFile
                 txtPath.Text = folderPath;
                 DirectoryInfo di = new DirectoryInfo(folderPath);
                 //copy file
-                foreach (var fi in di.GetFiles("*.txt", SearchOption.AllDirectories))
-                {
-                    msg(fi.FullName);
-                    msg(fi.Name);
-                }
+                searchFile("*.avi",di,ref listFile);
+                searchFile("*.mp4",di,ref listFile);                
             }
-            
+            dg1.DataSource = listFile;
+            insertData(listFile);
         }
+        /// <summary>
+        /// 擷取檔案列表
+        /// </summary>
+        /// <param name="extendName">副檔名</param>
+        /// <param name="di">目錄結構</param>
+        /// <param name="listFile">資料集合</param>
+        private void searchFile(string extendName, DirectoryInfo di, ref List<fileData> listFile)
+        {
+            foreach (var fi in di.GetFiles(extendName, SearchOption.AllDirectories))
+            {
+                fileData fd = new fileData()
+                {
+                    sourceName = fi.Name,
+                    fileName = getShortName(fi.Name),
+                    fileTime = fi.CreationTime.ToString(),
+                    location = fi.FullName
+                };
+                listFile.Add(fd);
+                //msg(fi.FullName);
+                //msg(fi.Name);
+            }
+        }
+        /// <summary>
+        /// 取得短名字
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        private string getShortName(string fileName)
+        {
+            string input = fileName;
+            if (input.IndexOf("720p") >= 0 || input.IndexOf("1080p") >= 0)
+                return input;
+            string urlPattern = @"^.+(.net|.com|.cc|.co)[^a-zA-Z0-9]?"; //網址
+            string pattern = @"[a-zA-Z]{2,}-{0,}\d{2,}";
+                // 先判斷是否有網址?網址取代:繼續
+            Regex urlrgx = new Regex(urlPattern, RegexOptions.IgnoreCase);
+            MatchCollection urlmatches = urlrgx.Matches(fileName);
+            if (urlmatches.Count > 0)
+            {
+                foreach (Match match in urlmatches)
+                    input = fileName.Replace(match.Value, "");
+            }
+
+            //取檔案名
+            Regex rgx = new Regex(pattern, RegexOptions.IgnoreCase);
+            MatchCollection matches = rgx.Matches(input);
+            if (matches.Count > 0)
+            {                    
+                foreach (Match match in matches)                    
+                    return match.Value.Replace("-","");                    
+            }   
+            return input;
+        }
+
+        private void insertData(List<fileData> fd)
+        {
+            using (sqlite_connect = new SQLiteConnection("Data source = fileLibary.db"))
+            {
+                //建立資料庫連線                
+                sqlite_connect.Open();// Open
+                sqlite_cmd = sqlite_connect.CreateCommand();
+                SQLiteTransaction sqlite_trans = sqlite_connect.BeginTransaction();
+                foreach(var row in fd)
+                {
+                    sqlite_cmd.CommandText = string.Format(@"INSERT INTO movie 
+                        VALUES (null,'{0}','{1}','{2}','{3}','{4}');",row.sourceName,row.fileName,row.location,row.fileTime,DateTime.Now);
+                    sqlite_cmd.ExecuteNonQuery();
+                }
+                sqlite_trans.Commit();
+            }
+        }
+        private void msg(string message)
+        {
+            txtMsg.Text += Environment.NewLine + message;
+            txtMsg.SelectionStart = txtMsg.Text.Length;
+            txtMsg.ScrollToCaret();
+        }
+       
+
+        private async Task<IEnumerable<fileData>> searchKeyword(string keyword)
+        {
+            keyword = "%" + keyword + "%";
+
+            using (var conn = new SQLiteConnection("Data source = fileLibary.db"))
+            {
+                conn.Open();
+                var result = await conn.QueryAsync<fileData>(@"select * from movie where fileName like @keyword;", new { keyword });
+                return result;
+            }
+        }
+        #region 測試
+        /// <summary>
+        /// 文字檔測試用
+        /// </summary>
         public void testRegex()
         {
             List<fileData> listFile = new List<fileData>();
 
             OpenFileDialog openFileDg = new OpenFileDialog();
-            if(openFileDg.ShowDialog() == DialogResult.OK)
+            if (openFileDg.ShowDialog() == DialogResult.OK)
             {
                 var fileName = Path.Combine(openFileDg.FileName);
                 StreamReader sr = new StreamReader(fileName);
@@ -84,11 +289,11 @@ namespace NoticeRepeatFile
                     // 先判斷是否有網址?網址取代:繼續
                     Regex urlrgx = new Regex(urlPattern, RegexOptions.IgnoreCase);
                     MatchCollection urlmatches = urlrgx.Matches(input);
-                    if(urlmatches.Count> 0)
+                    if (urlmatches.Count > 0)
                     {
                         foreach (Match match in urlmatches)
                             input = input.Replace(match.Value, "");
-                    }   
+                    }
 
                     //取檔案名
                     Regex rgx = new Regex(pattern, RegexOptions.IgnoreCase);
@@ -98,17 +303,18 @@ namespace NoticeRepeatFile
                         //Console.WriteLine("{0} ({1} matches):", input, matches.Count);
                         foreach (Match match in matches)
                         {
-                            fileData fd = new fileData() {
+                            fileData fd = new fileData()
+                            {
                                 sourceName = input,
                                 fileName = match.Value,
                                 fileTime = "2016/12/30",
                                 location = fileName
                             };
-                            listFile.Add(fd);                         
+                            listFile.Add(fd);
                         }
                     }
                     else
-                    {                        
+                    {
                         fileData fd = new fileData()
                         {
                             sourceName = input,
@@ -127,60 +333,12 @@ namespace NoticeRepeatFile
 
 
         }
-        private void insertData(List<fileData> fd)
-        {
-            using (sqlite_connect = new SQLiteConnection("Data source = fileLibary.db"))
-            {
-                //建立資料庫連線                
-                sqlite_connect.Open();// Open
-                sqlite_cmd = sqlite_connect.CreateCommand();
-                SQLiteTransaction sqlite_trans = sqlite_connect.BeginTransaction();
-                foreach(var row in fd)
-                {
-                    sqlite_cmd.CommandText = string.Format(@"INSERT INTO movie 
-                        VALUES (null,'{0}','{1}','{2}','{3}','{4}');",row.sourceName,row.fileName,row.location,row.fileName,DateTime.Now);
-                    sqlite_cmd.ExecuteNonQuery();
-                }
-                sqlite_trans.Commit();
-            }
-        }
-        private void msg(string message)
-        {
-            txtMsg.Text += Environment.NewLine + message;
-            txtMsg.SelectionStart = txtMsg.Text.Length;
-            txtMsg.ScrollToCaret();
-        }
-        private void button1_Click(object sender, EventArgs e)
-        {
-            //replaceFile();
-            testRegex();
-        }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            //string txtKeyWord = txtKey.Text;
-            //string[] keyword = txtKeyWord.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
-            //foreach (var a in keyword)
-            //{
-            //listMsg.Items.Add(a);
-            string a = "";
-                var result = searchKeyword(a);
-                if (result.Result.Count() > 0)
-                    dg1.DataSource = result.Result.ToList();
 
-            //}
-        }
 
-        private async Task<IEnumerable<fileData>> searchKeyword(string keyword)
-        {
-            keyword = "%" + keyword + "%";
 
-            using (var conn = new SQLiteConnection("Data source = fileLibary.db"))
-            {
-                conn.Open();
-                var result = await conn.QueryAsync<fileData>(@"select * from movie where fileName like @keyword;", new { keyword });
-                return result;
-            }
-        }
+        #endregion
+
+ 
     }
 }
